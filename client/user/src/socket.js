77a7a07d28587e79691bff000c680c84245e8201
Roman <rsamarar13@gmail.com>
2024-10-10 02:13:44 +0300

    Docker compose fix

--- client/user/src/socket.js
+++ client/user/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const socketConnect = () => {
-	const URL = 'http://server-container:4000';
+	const URL = 'http://localhost:4000';
 	const socket = io(URL, { autoConnect: false });
 
 	// socket.onAny((event, ...args) => {
@@ -11,7 +11,6 @@ export const socketConnect = () => {
 	socket.on('connect_error', (err) => {
 		console.error('Error while creating a socket connection:', err);
 	});
-
 	socket.connect();
 	return socket;
 };
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    // add multiple origins here
+    origin: ['http://localhost:3000', 'http://localhost:5137'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(4000);
 }
 bootstrap();
