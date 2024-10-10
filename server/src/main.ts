97e0b504c1e89d82d2e3a33e84d4cad93b9c1494
Kakashius <nakadashi@mail.ru>
2024-10-10 23:59:26 +0300

    correct ports

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     // add multiple origins here
-    origin: ['http://localhost:3000', 'http://localhost:5137'],
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   await app.listen(4000);
